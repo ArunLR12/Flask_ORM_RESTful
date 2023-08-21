70ec1830b9b5ccc6525d9dc5a9498bbf71e84f56
ArunLR12 <142532517+ArunLR12@users.noreply.github.com>
2023-08-21 19:27:00 +0530

    Update routers.py

--- demoApp/routers/routers.py
+++ demoApp/routers/routers.py
@@ -3,32 +3,22 @@ from models.models import Invoice, InvoiceItem
 from settings.settings import SECRET_KEY, SQLALCHEMY_DATABASE_URI
 from app.app import jsonify,request
 
-
-
+    
 @app.route('/create_invoice', methods=['POST'])
 def create_invoice():
-    data = request.json  # JSON data has to be sent in the request body
-    provided_invoice_id = data.get('invoice_id')  # Extract custom invoice ID
-
-    # Check if the provided invoice ID already exists
-    existing_invoice = Invoice.query.get(provided_invoice_id)
-    if existing_invoice:
-        return "Invoice ID already exists"
-
-    new_invoice = Invoice(id=provided_invoice_id, date=date.today())
+    new_invoice = Invoice(date=date.today())
     db.session.add(new_invoice)
     db.session.commit()
     return "Invoice created successfully"
-    
 
-@app.route('/add_invoice_item/<int:invoice_id>', methods=['POST'])
+
+@app.route('/api/invoice/<int:invoice_id>/invoice_item', methods=['POST'])
 def add_invoice_item(invoice_id):
     invoice = Invoice.query.get(invoice_id)
     if not invoice:
-        return "Invoice not found", 404  # Return a 404 status code for not found
+        return "Invoice not found", 404
     
     if request.method == 'POST':
-        print(request.form)
         data = request.form
         item = InvoiceItem(
             units=data['units'],
@@ -44,6 +34,8 @@ def add_invoice_item(invoice_id):
     return "Invalid request", 400
 
 
+
+
 #Below 2 functions are created for some endpoints to demonstrate and GET the created data. It demonstrates RESTful API.
 
 @app.route('/invoices', methods=['GET'])
@@ -51,13 +43,24 @@ def get_invoices():
     invoices = Invoice.query.all()
     invoice_list = []
     for invoice in invoices:
-        invoice_list.append({
+        invoice_data = {
             'id': invoice.id,
-            'date': invoice.date.strftime('%Y-%m-%d')
-        })
+            'date': invoice.date.strftime('%Y-%m-%d'),
+            'items': []
+        }
+        for item in invoice.items:
+            item_data = {
+                'id': item.id,
+                'units': item.units,
+                'description': item.description,
+                'amount': str(item.amount)
+            }
+            invoice_data['items'].append(item_data)
+        invoice_list.append(invoice_data)
     return jsonify(invoice_list)
 
-@app.route('/invoice_items/<int:invoice_id>', methods=['GET'])
+
+@app.route('/api/invoice/<int:invoice_id>/invoice_items', methods=['GET'])
 def get_invoice_items(invoice_id):
     invoice = Invoice.query.get(invoice_id)
     if not invoice:
