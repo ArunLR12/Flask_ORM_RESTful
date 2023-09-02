e47fe2e85b34b95e7b72b80d11798bdb25dabb75
ArunLR12 <142532517+ArunLR12@users.noreply.github.com>
2023-09-02 19:33:05 +0530

    Update controller.py

--- demoApp/controller.py
+++ demoApp/controller.py
@@ -5,4 +5,4 @@ from routers.routers import create_invoice,add_invoice_item,get_invoices,get_inv
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='db',debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
